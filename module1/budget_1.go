6d2fbfcc53dde1fdb0015304c6120986ac06a892
federico.moreno1 <federico.moreno@kavak.com>
2023-02-06 10:24:27 -0300

    dont know the error :(

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,8 +1,13 @@
 package module1
 
+
 // Budget stores budget information
-Budget struct {Max float32;
-Items []Item}
+Budget struct {
+Max float32;
+Items []Item
+}
 // Item stores item information
-Item struct {Description string;
-Price float32}
+Item struct {
+Description string;
+Price float32
+}
