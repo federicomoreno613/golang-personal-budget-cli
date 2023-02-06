3e5429fd2433b6bdc6cc7efaba0213b344aac260
federico.moreno1 <federico.moreno@kavak.com>
2023-02-06 10:19:37 -0300

    Defining Structs

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,8 @@
 package module1
 
 // Budget stores budget information
-
+Budget struct {Max float32;
+Items []Item}
 // Item stores item information
+Item struct {Description string;
+Price float32}
